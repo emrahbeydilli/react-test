7923e05080c954b19aaa500af811706bcca081bc
emrahbeydilli <emrahbeydilli@gmail.com>
2022-04-17 13:43:23 +0300

    adding startup files

---
+++ src/Hello.jsx
@@ -0,0 +1,10 @@
+import './Hello.css';
+const Hello = (props) => {
+    return (
+        <div className='f1 tc'>
+            <h1>Hello World!</h1>
+            <p>{props.greeting}</p>
+        </div>
+    );
+}
+export default Hello;
